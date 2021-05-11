7fc99e5d0338977dc9a40642b12fe95b3b51bb0f
cocoloring <cocoloring@outlook.com>
2021-05-11 23:29:16 +0200

    feat(utils.char): new class for Char

---
+++ packages/utils/src/Char.ts
@@ -0,0 +1,51 @@
+import Float from './Float'
+
+export default class Char {
+    protected data: number
+
+    constructor(initCharCode = 0x20) {
+        this.data = this.formatData(initCharCode)
+    }
+
+    get value(): number {
+        return this.data
+    }
+
+    set value(value: number) {
+        this.data = this.formatData(value)
+    }
+
+    protected formatData(data: number): number {
+        // TODO: should as a methods of Float
+        return Math.floor(Math.max(0, data))
+    }
+
+    toFloat(): Float {
+        return new Float(this.value)
+    }
+
+    valueOf(): number {
+        return this.value
+    }
+
+    protected splitNumberAsArray(valueTotal: number, valueMax = 10): number[] {
+        // TODO: as a methods of Integer
+        const values: number[] = []
+        let valueRemain = valueTotal
+        while (valueRemain > 0) {
+            values.unshift(valueRemain % valueMax)
+            valueRemain = Math.floor(valueRemain / valueMax)
+        }
+        return values
+    }
+
+    toString(): string {
+        return String.fromCharCode(
+            ...this.splitNumberAsArray(this.value, 0x10000),
+        )
+    }
+
+    clone(): Char {
+        return new Char(this.value)
+    }
+}

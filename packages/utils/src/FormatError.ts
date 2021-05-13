31dde364a46681514e12534aae94ff613efb4427
cocoloring <cocoloring@outlook.com>
2021-05-13 15:03:09 +0200

    feat(utils.formaterror): add errors

--- packages/utils/src/FormatError.ts
+++ packages/utils/src/FormatError.ts
@@ -1,6 +1,9 @@
 enum ErrorText {
     BadString = 'string cannot be parsed',
     BadCanvasContext2D = 'unable to get context 2d from a html canvas element',
+    NoConstructor = "it's not allowed to create a instances of this class via constructor.",
+    BufferedItemRepeatSet = 'BufferedItem can be set once only',
+    BufferedItemNotSet = 'BufferedItem must be set first',
 }
 
 export default class FormatError extends Error {

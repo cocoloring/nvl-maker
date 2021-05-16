1e3dac08c027834a1aea1ca4b8ec039022576a43
cocoloring <cocoloring@outlook.com>
2021-05-16 21:20:21 +0200

    feat: impl class Integer

---
+++ packages/utils/src/Integer.ts
@@ -0,0 +1,67 @@
+import FormatError from './FormatError.js'
+
+export default class Integer {
+    static fromNumber(n: number): Integer {
+        return new Integer(+n)
+    }
+
+    static fromString(n: string): Integer {
+        const ref = Math.floor(+n)
+        if (isNaN(ref)) {
+            throw new FormatError('BadString')
+        } else {
+            return new Integer(ref)
+        }
+    }
+
+    protected data: number
+
+    constructor(initValue = 0) {
+        this.data = initValue
+    }
+
+    get value(): number {
+        return Math.floor(this.data)
+    }
+
+    set value(value: number) {
+        this.data = value
+    }
+
+    plus(n: Integer): this {
+        this.value += n.value
+        return this
+    }
+
+    minus(n: Integer): this {
+        this.value -= n.value
+        return this
+    }
+
+    times(n: Integer): this {
+        this.value *= n.value
+        return this
+    }
+
+    divides(n: Integer): this {
+        this.value = n.value / this.value
+        return this
+    }
+
+    dividedBy(n: Integer): this {
+        this.value /= n.value
+        return this
+    }
+
+    valueOf(): number {
+        return this.value
+    }
+
+    toString(): string {
+        return String(this.value)
+    }
+
+    clone(): Integer {
+        return new Integer(this.value)
+    }
+}

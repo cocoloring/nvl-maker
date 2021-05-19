6b0d5137ad34234252d880921595b7cecaf236cb
cocoloring <cocoloring@outlook.com>
2021-05-19 20:54:23 +0200

    feat(utils): export all classes

--- packages/utils/src/main.ts
+++ packages/utils/src/main.ts
@@ -1,7 +1,15 @@
-import Number from './Float.js'
+import Float from './Float.js'
+import Integer from './Integer.js'
+import Char from './Char.js'
+import BufferedItem from './BufferedItem.js'
+import Vector2 from './Vector2.js'
 import FormatError from './FormatError.js'
 
 export default class Utils {
-    static Number = Number
+    static Float = Float
+    static Integer = Integer
+    static Char = Char
+    static BufferedItem = BufferedItem
     static FormatError = FormatError
+    static Vector2 = Vector2
 }

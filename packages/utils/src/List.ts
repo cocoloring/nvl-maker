d4e7aee9ee9d21110012706830448e15a2ba77d0
cocoloring <cocoloring@outlook.com>
2021-05-21 16:00:17 +0200

    feat(utils.list): add methods for list motification

--- packages/utils/src/List.ts
+++ packages/utils/src/List.ts
@@ -13,6 +13,46 @@ export default class List<T> implements BasicObject<List<T>>, Iterable<T> {
         this.data = []
     }
 
+    get length(): number {
+        return this.data.length
+    }
+
+    add(item: T, position: number = this.data.length - 1): this {
+        const lastPosition = this.data.length - 1
+
+        if (position === 0) {
+            this.data.unshift(item)
+        } else if (position === lastPosition) {
+            this.data.push(item)
+        } else {
+            this.data.splice(position, 0, item)
+        }
+        return this
+    }
+
+    addMany(items: Array<T>, position: number = this.data.length - 1): this {
+        this.data.splice(position, 0, ...items)
+        return this
+    }
+
+    remove(position: number = this.data.length - 1): this {
+        const lastPosition = this.data.length - 1
+
+        if (position === 0) {
+            this.data.shift()
+        } else if (position === lastPosition) {
+            this.data.pop()
+        } else {
+            this.data.splice(position, 1)
+        }
+        return this
+    }
+
+    removeMany(howMany: number, position: number = this.data.length - 1): this {
+        this.data.splice(position, howMany)
+        return this
+    }
+
     [Symbol.iterator](): Iterator<T> {
         return this.data[Symbol.iterator]()
     }

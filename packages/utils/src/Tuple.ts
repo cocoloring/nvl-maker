fedff0303951906e4b222377f597ab668e4abeeb
cocoloring <cocoloring@outlook.com>
2021-05-23 23:03:10 +0200

    feat(utils.tuple): impl. class Tuple

---
+++ packages/utils/src/Tuple.ts
@@ -0,0 +1,51 @@
+import type { BasicObject } from './BasicType'
+
+export default class Tuple<T> implements BasicObject<Tuple<T>>, Iterable<T> {
+    static fromIterable<T>(iter: Iterable<T>): Tuple<T> {
+        const tuple = new Tuple<T>(...iter)
+        return tuple
+    }
+
+    protected data: Array<T>
+    readonly length: number
+
+    constructor(...items: T[]) {
+        this.length = items.length
+        this.data = [...items]
+    }
+
+    set(item: T, position: number): this {
+        if (position > 0 && position < this.length) {
+            this.data[position] = item
+        } else {
+            // TODO: costume error
+            throw new Error('Range Error')
+        }
+        return this
+    }
+
+    get(position: number): T {
+        if (position > 0 && position < this.length) {
+            return this.data[position]
+        } else {
+            // TODO: costume error
+            throw new Error('Range Error')
+        }
+    }
+
+    [Symbol.iterator](): Iterator<T> {
+        return this.data[Symbol.iterator]()
+    }
+
+    clone(): Tuple<T> {
+        return new Tuple(...this.data)
+    }
+
+    toString(): string {
+        return this.data.map((item) => String(item)).join(',')
+    }
+
+    valueOf(): number {
+        return this.data.length
+    }
+}

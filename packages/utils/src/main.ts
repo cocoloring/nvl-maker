27cdd6abdcc9714e7d7e3b18da740aedfa49a500
cocoloring <cocoloring@outlook.com>
2021-05-30 16:53:16 +0200

    refactor(utils): use object constant instead of class

--- packages/utils/src/main.ts
+++ packages/utils/src/main.ts
@@ -3,13 +3,30 @@ import Integer from './Integer.js'
 import Char from './Char.js'
 import BufferedItem from './BufferedItem.js'
 import Vector2 from './Vector2.js'
+import List from './List.js'
+import Tuple from './Tuple.js'
 import FormatError from './FormatError.js'
+import Text from './Text.js'
 
-export default class Utils {
-    static Float = Float
-    static Integer = Integer
-    static Char = Char
-    static BufferedItem = BufferedItem
-    static FormatError = FormatError
-    static Vector2 = Vector2
+const Utils = {
+    // basic value type
+    Integer,
+    Float,
+    Char,
+
+    // data structure
+    List,
+    Tuple,
+
+    // combined value type
+    Text,
+    Vector2,
+
+    // utils functions
+    BufferedItem,
+
+    // error
+    FormatError,
 }
+
+export default Utils

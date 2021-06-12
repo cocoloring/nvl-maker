0bb868ed8889eff2ac8353214c48166233e5c3bb
cocoloring <cocoloring@outlook.com>
2021-06-12 16:06:41 +0200

    refactor(types): separate interface

--- packages/types/src/BitOperable.ts
+++ packages/types/src/BitOperable.ts
@@ -1,8 +1,27 @@
-export interface BitOperable<T> {
+export interface BitNotOperable {
     not(): this
+}
+
+export interface BitOrOperable<T> {
     or(value: T): this
+}
+
+export interface BitXorOperable<T> {
     xor(value: T): this
+}
+
+export interface BitAndOperable<T> {
     and(value: T): this
+}
+
+export interface BitShiftOperable {
     leftShift(arithmetic?: boolean): this
     rightShift(arithmetic?: boolean): this
 }
+
+export interface BitOperable<T>
+    extends BitNotOperable,
+        BitOrOperable<T>,
+        BitXorOperable<T>,
+        BitAndOperable<T>,
+        BitShiftOperable {}

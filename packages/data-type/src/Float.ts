ab30518b0b5d3e9a86da596b3520ccb9aca49586
cocoloring <cocoloring@outlook.com>
2021-07-04 12:31:49 +0200

    chore: merge

--- packages/data-type/src/Float.ts
+++ packages/data-type/src/Float.ts
@@ -2,7 +2,8 @@ import { FormatError } from '@nvl-maker/error'
 import type { BasicObject, AdditionalNumericArithmetic } from './types/main'
 
 export class Float
-    implements BasicObject<Float>, AdditionalNumericArithmetic<Float> {
+    implements BasicObject<Float>, AdditionalNumericArithmetic<Float>
+{
     static E = new Float(Math.E)
     static Pi = new Float(Math.PI)
 

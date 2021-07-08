2a5d3cafba8d8c056db103fce2602b04d020e484
cocoloring <cocoloring@outlook.com>
2021-07-08 17:16:12 +0200

    feat(data-type/char): change a lot, remove graphic measurement function
    BREAKING CHANGE: get desplaySize(), toFloat() removed. get/set value() modified. constructor will
    use number[] as param now. add static methods fromString and fromArray

--- packages/data-type/src/Char.ts
+++ packages/data-type/src/Char.ts
@@ -1,102 +1,59 @@
-import { BufferedItem } from '@nvl-maker/utils'
-import { FormatError } from '@nvl-maker/error'
 import type { BasicObject } from './types/main'
+import { FormatError } from '@nvl-maker/error'
 
-import { Float } from './Float'
-import { Vector2 } from './Vector2'
-import { Integer } from './Integer'
+export class Char implements BasicObject<Char> {
+    protected static readonly defaultCharCode = [0x20]
 
-import { createCanvas } from 'canvas'
+    protected isStringBufferShouldUpdate = true
+    protected stringBuffer = ''
 
-const bufferName = {
-    canvas: 'char_canvas_for_char_measure',
-} as const
+    protected data: Uint16Array
 
-BufferedItem.set(bufferName.canvas, () => {
-    let canvas: unknown
-    if (typeof window === 'object') {
-        canvas = document.createElement('canvas')
-    } else {
-        canvas = createCanvas(200, 200)
-    }
-    return canvas as HTMLCanvasElement
-})
-
-export class Char extends Integer implements BasicObject<Char> {
-    constructor(initCharCode = 0x20) {
-        super()
-        this.data = this.formatData(initCharCode)
+    constructor(...initCharCode: number[]) {
+        // TODO: need cut the first character
+        this.data = new Uint16Array(initCharCode)
+        this.calcDataToString()
     }
 
-    set value(value: number) {
-        const ref = this.formatData(value)
-        if (this.data !== ref) {
-            this.isValueChanged = true
-            this.data = ref
+    static fromString(str: string): Char {
+        if (str.length <= 0) {
+            throw new FormatError('BadString')
         }
+        return new Char(...str.split('').map((str) => str.charCodeAt(0)))
+    }
+
+    static fromArray(array: Iterable<number>): Char {
+        return new Char(...array)
+    }
+
+    set value(value: number[] | Uint16Array) {
+        this.data = new Uint16Array(value)
+        this.isStringBufferShouldUpdate = true
+    }
+
+    get value(): Uint16Array {
+        return this.data
     }
 
     get length(): number {
-        // how many bites
-        return Math.floor(Math.log(this.data) / Math.log(0x100))
+        return this.data.length
     }
 
-    protected formatData(data: number): number {
-        // TODO: should as a methods of Float
-        return Math.floor(Math.max(0, data))
+    protected calcDataToString(): void {
+        this.stringBuffer = String.fromCodePoint(...this.data)
+        this.isStringBufferShouldUpdate = false
     }
 
-    toFloat(): Float {
-        return new Float(this.value)
-    }
-
-    protected bufferString = ''
-
-    protected calcString(): void {
-        this.bufferString = String.fromCharCode(
-            ...new Integer(this.value).split().map((i) => i.valueOf()),
-        )
+    valueOf(): string {
+        return this.toString()
     }
 
     toString(): string {
-        if (this.isValueChanged) this.calcString()
-        return this.bufferString
+        if (this.isStringBufferShouldUpdate) this.calcDataToString()
+        return this.stringBuffer
     }
 
     clone(): Char {
-        // TODO: maybe copy all buffered values?
-        const c = new Char(this.value)
-        c.bufferedDisplaySize = this.bufferedDisplaySize.clone()
-        c.isValueChanged = this.isValueChanged
-        return c
-    }
-
-    // TODO: need give it a setter
-    protected fontName = '10px sans-serif'
-
-    protected isValueChanged = true
-
-    protected calcDisplaySize(): void {
-        const canvas = BufferedItem.get<HTMLCanvasElement>(bufferName.canvas)
-        const context2D = canvas.getContext('2d')
-        if (context2D === null) throw new FormatError('BadCanvasContext2D')
-        context2D.font = this.fontName
-        const {
-            width,
-            actualBoundingBoxAscent,
-            actualBoundingBoxDescent,
-        } = context2D.measureText(this.toString())
-        console.log(context2D.measureText(this.toString()))
-        this.bufferedDisplaySize.value = [
-            width,
-            actualBoundingBoxAscent + actualBoundingBoxDescent,
-        ]
-    }
-
-    protected bufferedDisplaySize: Vector2 = new Vector2()
-
-    get displaySize(): Vector2 {
-        if (this.isValueChanged) this.calcDisplaySize()
-        return this.bufferedDisplaySize
+        return new Char(...this.data)
     }
 }

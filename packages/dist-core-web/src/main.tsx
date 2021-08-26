60b197f6d6ced445a763ba5134e564adeb937ec1
cocoloring <cocoloring@outlook.com>
2021-08-26 17:30:30 +0200

    refactor(dist-core-web): remove root div in html template

--- packages/dist-core-web/src/main.tsx
+++ packages/dist-core-web/src/main.tsx
@@ -2,5 +2,8 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
 import { App } from './App'
-
-ReactDOM.render(<App />, document.querySelector('#root'))
+;(function main(): void {
+    const rootElement = document.createElement('div')
+    document.body.appendChild(rootElement)
+    ReactDOM.render(<App />, rootElement)
+})()

fdc945d40ab3ba4097395ade4e7fac00c4c1eb18
cocoloring <cocoloring@outlook.com>
2021-09-10 21:50:44 +0200

    chore: merge from remote

--- packages/core/src/component/CoreContainer.tsx
+++ packages/core/src/component/CoreContainer.tsx
@@ -3,13 +3,31 @@ import { Canvas } from '@react-three/fiber'
 import { Box } from './Box'
 
 export enum Platform {
-    Web,
-    Electron,
-    WebFullPage,
+    Web, // on websites
+    Desktop, // on desktop PCs or
+    Phone, // on phones like iPhone or android phone
+    Tablet, // on tablet PCs like iPad or all android tablets
+}
+
+export enum OS {
+    Window, // window
+    MacOS, // on macOS
+    Android, // on Android phone or Android tablet
+    IOS, // iOS system, like iPhone or iOS
+}
+
+export enum WindowMode {
+    Windowed, // desktop only
+    FullScreen, // desktop, phone, tablet only
+    VerticalFullScreen, // phone, tablet
+    AsElement, // web only
+    AsFullScreenElement, // web only
 }
 
 export interface CoreConfig {
     platform?: Platform
+    os?: OS
+    windowMode?: WindowMode
 }
 
 export function CoreContainer(param?: CoreConfig): JSX.Element {
@@ -23,4 +41,8 @@ export function CoreContainer(param?: CoreConfig): JSX.Element {
             <Box position={[1.2, 0, 0]} />
         </Canvas>
     )
+}
+
+export interface A {
+    a?: { [key: string]: number }
 }

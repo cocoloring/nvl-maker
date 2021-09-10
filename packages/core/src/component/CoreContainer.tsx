441f6880e18482a868d2fe74bfb4656a1217c8d4
cocoloring <cocoloring@outlook.com>
2021-09-10 19:14:33 +0200

    feat(core): add more enum prepare for options

--- packages/core/src/component/CoreContainer.tsx
+++ packages/core/src/component/CoreContainer.tsx
@@ -1,16 +1,39 @@
 import * as React from 'react'
 
 export enum Platform {
-    Web,
-    Electron,
+    Web, // on websites
+    Desktop, // on desktop PCs or
+    Phone, // on phones like iPhone or android phone
+    Tablet, // on tablet PCs like iPad or all android tablets
+}
+
+export enum OS {
+    Window, // window
+    MacOS, // on macOS
+    Android, // on Android phone or Android tablet
+    IOS, // iOS system, like iPhone or iOS
+}
+
+export enum WindowMode {
+    Windowed, // desktop only
+    FullScreen, // desktop, phone, tablet only
+    VerticalFullScreen, // phone, tablet
+    AsElement, // web only
+    AsFullScreenElement, // web only
 }
 
 export interface CoreConfig {
     platform?: Platform
+    os?: OS
+    windowMode?: WindowMode
 }
 
 export function CoreContainer(param?: CoreConfig): JSX.Element {
     const options: CoreConfig = { ...param } ?? {}
     options.platform ??= Platform.Web
     return <div>Hello, NVL Maker</div>
+}
+
+export interface A {
+    a?: { [key: string]: number }
 }

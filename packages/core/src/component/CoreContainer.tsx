36bfaa04f9fa368669645e79a9bb863590623274
cocoloring <cocoloring@outlook.com>
2021-09-11 22:16:46 +0200

    feat(core): set default window mode as full screen

--- packages/core/src/component/CoreContainer.tsx
+++ packages/core/src/component/CoreContainer.tsx
@@ -28,17 +28,63 @@ export interface CoreConfig {
     platform?: Platform
     os?: OS
     windowMode?: WindowMode
+    windowSize?: [number, number]
+}
+
+type Complete<T> = {
+    [P in keyof Required<T>]: Pick<T, P> extends Required<Pick<T, P>>
+        ? T[P]
+        : T[P] | undefined
+}
+
+export function coreConfigInitialization(
+    options?: CoreConfig,
+): Complete<CoreConfig> {
+    const opts: CoreConfig = { ...options } ?? {}
+    opts.os ??= OS.Window
+    opts.windowSize ??= [0, 0]
+    opts.windowSize[0] ??= 0
+    opts.windowSize[1] ??= 0
+    opts.platform ??= Platform.Web
+    switch (opts.platform) {
+        case Platform.Web: {
+            opts.windowMode ??= WindowMode.AsFullScreenElement
+            break
+        }
+        case Platform.Desktop: {
+            opts.windowMode ??= WindowMode.FullScreen
+            break
+        }
+        case Platform.Web: {
+            opts.windowMode ??= WindowMode.AsFullScreenElement
+            break
+        }
+        case Platform.Desktop: {
+            opts.windowMode ??= WindowMode.FullScreen
+            break
+        }
+        default: {
+        }
+    }
+    return opts as unknown as Complete<CoreConfig>
 }
 
 export function CoreContainer(param?: CoreConfig): JSX.Element {
-    const options: CoreConfig = { ...param } ?? {}
-    options.platform ??= Platform.Web
+    const options = coreConfigInitialization(param)
     return (
-        <Canvas>
-            <ambientLight />
-            <pointLight position={[10, 10, 10]} />
-            <Box position={[-1.2, 0, 0]} />
-            <Box position={[1.2, 0, 0]} />
-        </Canvas>
+        <div
+            style={{
+                ...(options.windowMode === WindowMode.AsFullScreenElement
+                    ? { width: '100vw', height: '100vh' }
+                    : undefined),
+            }}
+        >
+            <Canvas>
+                <ambientLight />
+                <pointLight position={[10, 10, 10]} />
+                <Box position={[-1.2, 0, 0]} />
+                <Box position={[1.2, 0, 0]} />
+            </Canvas>
+        </div>
     )
 }
